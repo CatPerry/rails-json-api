bd6ea6842b5e229b376cdc8b9e497198f1729e8e
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-08-26 18:32:57 -0400

    Rails as API

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,52 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        articles = Article.order('created_at DESC');
+        render json: {status: 'SUCCESS', 
+                      message: 'Loaded articles successfully', 
+                      data: articles}, 
+        status: :ok
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'SUCCESS', 
+                      message: 'Loaded articles successfully', 
+                      data: article}, 
+        status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+
+        if article.save
+          render json: {status: 'SUCCESS', 
+                      message: 'Saved article successfully', 
+                      data: article}, 
+          status: :ok
+          else 
+            render json: {status: 'ERROR', 
+                      message: 'Article not saved', 
+                      data: article.errors}, 
+            status: :unprocessable_entity
+        end
+      end
+
+      def destroy 
+        article = Article.find(params[:id])
+        article.destroy
+        render json: {status: 'SUCCESS', 
+                      message: 'Article deleted successfully', 
+                      data: article}, 
+        status: :ok
+      end
+
+      private
+
+      def article_params
+        params.permit(:title, :body)
+      end
+    end
+  end
+end
